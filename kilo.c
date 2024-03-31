754dbc1a08a4293cc6f0519a616d78921ee3af76
HopoZ <phmath41@gmail.com>
2024-03-31 10:08:45 +0800

    回显字符

--- kilo.c
+++ kilo.c
@@ -1,27 +1,34 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 struct termios orig_termios;
 
-void disableRawMode(){
-	tcsetattr(STDIN_FILENO,TCSAFLUSH,&orig_termios);	
+void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &=
+        ~(ECHO |
+          ICANON); // the ICANON flag that allows us to turn off canonical mode.
+                   // This means we will finally be reading input byte-by-byte
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-
-void enableRawMode(){
-	tcgetattr(STDIN_FILENO,&orig_termios);
-	atexit(disableRawMode);
-
-	struct termios raw = orig_termios;
-	raw.c_lflag &= ~(ECHO);
-	tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
-}
-
-
-int main(){
-	enableRawMode();
-	char c;
-	while(read(STDIN_FILENO,&c,1)==1 && c !='q');
-	return 0;
+int main() {
+    enableRawMode();
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) {
+            printf("%d\n", c);
+        } else {
+            printf("%d ('%c')\n", c, c);
+        }
+    }
+    return 0;
 }
