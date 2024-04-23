212ed614d1f0daa9cfd68220c7c8a806388469d1
HopoZ <phmath41@gmail.com>
2024-04-23 16:58:19 +0800

    完善了viewer功能

--- hopoz.c
+++ hopoz.c
@@ -13,10 +13,13 @@
 #include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdarg.h>
 
 /*** defines ***/
 
 #define HOPOZ_VERSION "0.0.1"
+#define HOPOZ_TAB_STOP 8
 #define CTRL_KEY(k) ((k) & 0x1f) // define ctrl+character methods
 enum editorKey{
     ARROW_LEFT =1000, //We will give them a large integer value that is out of the range of a char, so that they don’t conflict with any ordinary keypresses.
@@ -34,14 +37,22 @@ enum editorKey{
 
 typedef struct erow{
     int size;
+    int rsize; //render size
     char *chars;
+    char* render;
 }erow; //editer's every row
 struct editorConfig {
-    int cx,cy;
+    int cx,cy; //locate the cursor
+    int rx; //render x
+    int rowoff; // row offset
+    int coloff;
     int screenrows;
     int screencols;
     int numrows;
     erow *row;
+    char *filename;
+    char statusmsg[80];
+    time_t statusmsg_time;
     struct termios orig_termios;
 };
 struct editorConfig E;
@@ -153,6 +164,39 @@ int getWindowsSize(int *rows, int *cols) {
 }
 
 /*** row operations ***/
+
+int editorRowCxToRx(erow *row, int cx){
+    int rx =0;
+    int j;
+    for(j =0;j<cx;j++){
+        if(row->chars[j]=='\t'){
+            rx +=(HOPOZ_TAB_STOP -1) -(rx%HOPOZ_TAB_STOP); // todo disunderstand
+        }
+        rx++;
+    }
+    return rx;
+}
+
+void editorUpdaterow(erow *row){
+    int tabs =0;
+    int j =0;
+    for(j =0;j <row->size;j++){
+        if(row->chars[j]=='\t') tabs++;
+    }
+    free(row->render);
+    row->render =malloc(row->size +tabs *(HOPOZ_TAB_STOP -1) +1);
+    int idx =0;
+    for(int j =0;j<row->size;j++){
+        if(row->chars[j]=='\t'){
+            row->render[idx++] =' ';
+            while(idx%HOPOZ_TAB_STOP !=0) row->render[idx++] =' ';
+        }else {
+            row->render[idx++] =row->chars[j];
+        }
+    }
+    row->render[idx] ='\0';
+    row->rsize =idx;
+}
 void editorAppendRow(char *s,size_t len){
     E.row =realloc(E.row,sizeof(erow)*(E.numrows +1));
     int at =E.numrows;
@@ -160,11 +204,19 @@ void editorAppendRow(char *s,size_t len){
     E.row[at].chars =malloc(len+1);
     memcpy(E.row[at].chars,s,len);
     E.row[at].chars[len] ='\0';
+    
+    E.row[at].rsize =0;
+    E.row[at].render =NULL;
+    editorUpdaterow(&E.row[at]);
+
     E.numrows++;
 }
 
 /*** file i/o ***/
 void editorOpen(char *filename){
+    free(E.filename);
+    E.filename =strdup(filename); // makes a copy of the given string, allocating the required memory and assuming you will free() that memory.
+
     FILE *fp =fopen(filename,"r");
     if(!fp) die("fopen");
     char *line =NULL;
@@ -202,39 +254,94 @@ void abFree(struct abuf *ab) { free(ab->b); }
 
 /*** output ***/
 
+void editorScroll(){
+    E.rx =0;
+    if(E.cy <E.numrows){
+        E.rx =editorRowCxToRx(&E.row[E.cy],E.cx);
+    }
+
+    if(E.cy <E.rowoff){
+        E.rowoff =E.cy;
+    }
+    if(E.cy >=E.rowoff +E.screenrows){
+        E.rowoff =E.cy -E.screenrows +1;
+    }
+    if(E.rx <E.rowoff){
+        E.coloff =E.rx;
+    }
+    if(E.rx >=E.coloff +E.screencols){
+        E.coloff =E.rx -E.screencols +1;
+    }
+}
+
 void editorDrawRows(struct abuf *ab) {
     int y;
     for (y = 0; y < E.screenrows; y++) {
-        if(y>=E.numrows){
-        if(E.numrows==0 && y==E.screenrows/3){
-            char welcome[80];
-            int welcomelen =snprintf(welcome,sizeof(welcome),
-              "HopoZ editor --version %s",HOPOZ_VERSION); //string n print format
-            if(welcomelen>E.screencols)
-                welcomelen =E.screencols;
-            int padding =(E.screencols -welcomelen)/2;
-            if(padding){
-                abAppend(ab,"~",1);
-                padding--;
-            }
-            while(padding--)
-                abAppend(ab," ",1);
-            abAppend(ab,welcome,welcomelen);
-        }else
+        int filerow =y +E.rowoff;
+        if(filerow >=E.numrows){
+            if(E.numrows==0 && y==E.screenrows/3){
+                char welcome[80];
+                int welcomelen =snprintf(welcome,sizeof(welcome),"HopoZ editor --version %s",HOPOZ_VERSION); //string n print format
+                if(welcomelen>E.screencols)
+                    welcomelen =E.screencols;
+                int padding =(E.screencols -welcomelen)/2;
+                if(padding){
+                   abAppend(ab,"~",1);
+                   padding--;
+                }
+                while(padding--)
+                 abAppend(ab," ",1);
+                abAppend(ab,welcome,welcomelen);
+            }else
             abAppend(ab, "~", 1);
-    }else{
-            int len =E.row[y].size;
+        }else{
+            int len =E.row[filerow].rsize -E.coloff;
+            if(len <0) len =0;
             if(len >E.screencols) len =E.screencols;
-            abAppend(ab,E.row[y].chars,len);
+            abAppend(ab,&E.row[filerow].render[E.coloff],len);
         }
 
         abAppend(ab, "\x1b[K",3); // The K command (Erase In Line) erases part of the current line. Its argument is analogous to the J command’s argument:
-        if (y < E.screenrows - 1) {
             abAppend(ab, "\r\n", 2);
-        }
     }
 }
+
+void editorDrawStatusBar(struct abuf *ab){
+    abAppend(ab,"\x1b[7m",4);
+    char status[80],rstatus[80];
+    int len =snprintf(status,sizeof(status),"%.20s - %d lines",
+        E.filename ? E.filename : "[No Name]",E.numrows);
+    int rlen =snprintf(rstatus,sizeof(rstatus),"%d/%d",
+                       E.cy+1,E.numrows);
+    if(len>E.screencols) len =E.screencols;
+    abAppend(ab,status,len);
+    while(len <E.screencols){
+        if(E.screencols -len ==rlen){
+            abAppend(ab,rstatus,rlen);
+            break;
+        }else{
+            abAppend(ab," ",1);
+            len++;
+        }
+    }
+    abAppend(ab,"\x1b[m",3); // switches back to normal formatting
+    // other m command: bold (1), underscore (4), blink (5), and inverted colors (7)
+    abAppend(ab,"\r\n",2);
+}
+
+void editorDrawMessageBar(struct abuf *ab){
+    abAppend(ab,"\x1b[K",3);
+    int msglen =strlen(E.statusmsg);
+    if(msglen >E.screencols) msglen =E.screencols;
+    if(msglen && time(NULL) -E.statusmsg_time <5){
+        abAppend(ab,E.statusmsg,msglen);
+    }
+}
+
 void editorRefreshScreen() {
+
+    editorScroll();
+
     struct abuf ab = ABUF_INIT;
 
     abAppend(&ab, "\x1b[?25l", 6);
@@ -249,9 +356,12 @@ void editorRefreshScreen() {
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
+    editorDrawStatusBar(&ab);
+    editorDrawMessageBar(&ab);
 
     char buf[32];
-    snprintf(buf,sizeof(buf),"\x1b[%d;%dH",E.cy+1,E.cx+1); // add 1 to E.cy and E.cx to convert from 0-indexed values to the 1-indexed values that the terminal uses.
+    snprintf(buf,sizeof(buf),"\x1b[%d;%dH",(E.cy -E.rowoff)+1,(E.rx -E.coloff)+1); // add 1 to E.cy and E.cx to convert from 0-indexed values 
+    // to the 1-indexed values that the terminal uses.
     abAppend(&ab,buf,strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
@@ -260,22 +370,46 @@ void editorRefreshScreen() {
     abFree(&ab);
 }
 
+void editorSetStatusMessage(const char *fmt,...){ // ... makes this function a variadic function
+    va_list ap; // ap: argument_pointer, fmt: format
+    va_start(ap,fmt);
+    vsnprintf(E.statusmsg,sizeof(E.statusmsg),fmt,ap);
+    va_end(ap);
+    E.statusmsg_time =time(NULL);
+}
+
 /*** input ***/
 
 void editorMoveCursor(int key){
+    erow *row =(E.cy >=E.numrows) ? NULL :&E.row[E.cy];
+
     switch(key){
      case ARROW_LEFT:
         if(E.cx!=0) E.cx--;
+        else if(E.cy >0){
+                E.cy--;
+                E.cx =E.row[E.cy].size;
+            }
         break;
      case ARROW_RIGHT:
-        if(E.cx !=E.screencols -1) E.cx++;
+    if(row && E.cx <row->size){
+        E.cx++;
+    }else if(row && E.cx==row->size){
+            E.cy++;
+            E.cx =0;
+        }
         break;
      case ARROW_UP:
       if(E.cy!=0) E.cy--;
       break;
      case ARROW_DOWN:
-      if(E.cy != E.screenrows -1) E.cy++;
+      if(E.cy < E.numrows) E.cy++;
       break;
+    }
+    row =(E.cy >=E.numrows) ? NULL :&E.row[E.cy];
+    int rowlen =row ?row->size:0;
+    if(E.cx>rowlen){
+        E.cx =rowlen;
     }
 }
 void editorProcessKeypress() {
@@ -290,11 +424,20 @@ void editorProcessKeypress() {
         E.cx =0;
         break;
     case END_KEY:
-        E.cx =E.screencols-1;
+            if(E.cy <E.numrows){
+                E.cx =E.row[E.cy].size;
+            }
         break;
     case PAGE_UP:
     case PAGE_DOWN:
             {
+                if(c==PAGE_UP){
+                    E.cy =E.rowoff;
+                }else if(c==PAGE_DOWN){
+                    E.cy =E.rowoff +E.screenrows -1;
+                    if(E.cy >E.numrows) E.cy =E.numrows;
+                }
+
                 int times =E.screenrows;
                 while(times--){
                     editorMoveCursor(c==PAGE_UP ?ARROW_UP :ARROW_DOWN);
@@ -313,9 +456,17 @@ void editorProcessKeypress() {
 /*** init ***/
 
 void initEditor() {
-    E.cx =0,E.cy =0,E.numrows =0;; //locate the cursor
+    E.cx =0,E.cy =0; 
+    E.rx =0;
+    E.numrows =0;
+    E.rowoff =0;
+    E.coloff =0;
     E.row =NULL;
+    E.filename =NULL;
+    E.statusmsg[0] ='\0';
+    E.statusmsg_time =0;
     if (getWindowsSize(&E.screenrows, &E.screencols) == -1) die("getWindowsSize");
+    E.screenrows -=2;
 }
 
 int main(int argc,char *argv[]) {
@@ -324,6 +475,8 @@ int main(int argc,char *argv[]) {
     if(argc >=2){
         editorOpen(argv[1]);
     }
+
+    editorSetStatusMessage("HELP: Ctrl-q to leave");
 
     while (1) {
         editorRefreshScreen();
